f51683d361c5774361495554b1589549a933476f
wenbin huang <ben@wenbins-MacBook-Pro.local>
2020-05-06 15:27:43 -0700

    add doc2pdf which is converting docx to pdf in mac os env

---
+++ doc2pdf.py
@@ -0,0 +1,36 @@
+##this can be only to used in Mac platform
+#- the textutil and cupsfilter command is supported by mac os
+import os
+
+
+import sys
+
+
+def doc2pdf(filename):
+
+    name = filename.split(".")
+    name = name[0]
+    htmlfile = "./" + name + ".html"
+    pdffile = "./" + name + ".pdf"
+    cmd = "textutil -convert html -output " + htmlfile + " " + filename
+
+    stream = os.popen(cmd)
+    print(stream.read())
+    cmd = "cupsfilter" + " " + htmlfile +  " > " + pdffile
+    stream = os.popen(cmd)
+    print(stream.read())
+
+    if os.path.exists("./" + htmlfile):
+        stream = os.popen("rm " + "./" + htmlfile)
+        print(stream.read())
+
+
+if __name__=="__main__":
+    if len(sys.argv) != 2:
+        print("doc to pdf, usage, please input file name ")
+    else:
+        doc2pdf(sys.argv[1])
+
+
+
+
